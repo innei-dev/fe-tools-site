9f6a168443298543e3481f4d06f92b575472c157
Innei <i@innei.in>
2023-11-23 23:40:23 +0800

    chore: update next
    Signed-off-by: Innei <i@innei.in>

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   output: 'standalone',
   productionBrowserSourceMaps: true,
 
@@ -11,4 +8,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
