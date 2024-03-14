86b8ba1190acd60f37e6635a9649e6e396d92c7f
Innei <i@innei.in>
2024-03-14 19:42:35 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- src/pages/tools/color/converter/Input.tsx
+++ src/pages/tools/color/converter/Input.tsx
@@ -2,34 +2,29 @@
 
 import * as Label from '@radix-ui/react-label'
 import { useEffect, useRef, useState } from 'react'
+import clsx from 'clsx'
+import Color from 'color'
+import ColorIo from 'colorjs.io'
+import { useAtom } from 'jotai'
 import { toast } from 'sonner'
-import type { ColorPalette } from '~/utils/color'
 import type { FC, MouseEventHandler } from 'react'
 
 import { Input } from '~/components/ui/Input'
 
-import {
-  colorsUpdateBatch,
-  defaultColorVariantMap,
-  resetColors,
-  useColorStore,
-} from './store'
+import { currentColorAtom } from './store'
 
+type SupportedColorType = 'hex' | 'rgb' | 'hsl' | 'lch' | 'oklch'
 export const ColorInput: FC<{
-  type: ColorPalette
-  validator: (val: string) => boolean
+  type: SupportedColorType
 }> = (props) => {
-  const { type, validator } = props
-  const value = useColorStore((state) => state[type])
+  const { type } = props
 
-  const [errorMessage, setErrorMessage] = useState('')
+  const [currentGlobalColor, setCurrentGlobalColor] = useAtom(currentColorAtom)
+  const [inputColor, setInputColor] = useState(() =>
+    colorToColorString(currentGlobalColor, type),
+  )
+
   const ref = useRef<HTMLInputElement>(null)
-
-  useEffect(() => {
-    if (!value) {
-      resetColors()
-    }
-  }, [value])
 
   const copyValue: MouseEventHandler<HTMLDivElement> = (e) => {
     const $el = e.target as HTMLButtonElement
@@ -39,14 +34,17 @@ export const ColorInput: FC<{
     navigator.clipboard.writeText(value)
   }
 
-  const fullColor = useColorStore((state) => (state as any)[`${type}Full`])
-  const shortColor = useColorStore((state) => (state as any)[`${type}Short`])
+  const [isError, setIsError] = useState(false)
 
-  const handleFocus = () => {
-    useColorStore.setState({
-      adjustType: type,
-    })
-  }
+  const currentGlobalColorString = colorToColorString(currentGlobalColor, type)
+
+  useEffect(() => {
+    if (!ref.current) return
+    // is Focus
+    if (ref.current === document.activeElement) return
+
+    setInputColor(currentGlobalColorString)
+  }, [currentGlobalColorString])
 
   return (
     <div className="grid w-full max-w-4xl items-center gap-1.5">
@@ -54,25 +52,27 @@ export const ColorInput: FC<{
       <div className="grid grid-cols-[1fr_2fr] gap-4 [&>*]:relative [&>*]:flex">
         <div className="flex flex-col">
           <Input
-            placeholder={defaultColorVariantMap[type]}
-            className="inline-block w-[300px]"
+            disabled={type === 'oklch'}
+            placeholder={`Typing ${type} color`}
+            className={clsx(
+              'inline-block w-[300px]',
+
+              isError && 'border-2 border-red-500',
+            )}
             type="text"
             id={type}
-            value={value}
+            value={inputColor}
             ref={ref}
-            onFocus={handleFocus}
-            onChange={(e) => {
-              const value = e.target.value
-              if (!validator(value)) {
-                setErrorMessage(`Invalid ${type} color`)
-              } else {
-                setErrorMessage('')
-              }
-              useColorStore.setState({ [type]: value })
+            onBlur={() => {
+              setInputColor(currentGlobalColorString)
             }}
-            onKeyUp={() => {
-              if (!errorMessage) {
-                colorsUpdateBatch(type, value)
+            onChange={(e) => {
+              setInputColor(e.target.value)
+              setIsError(false)
+              try {
+                setCurrentGlobalColor(parseColor(e.target.value, type))
+              } catch {
+                setIsError(true)
               }
             }}
           />
@@ -81,24 +81,62 @@ export const ColorInput: FC<{
         <div className="items-center">
           <div
             style={{
-              backgroundColor: fullColor,
+              backgroundColor: currentGlobalColorString,
             }}
-            className="h-4 w-4 rounded-full border border-black border-opacity-80"
+            className="h-4 w-4 rounded-full border border-black border-opacity-80 dark:border-white/90"
           />
 
-          {!errorMessage && (
-            <div
-              className="ml-4 grid flex-1 grid-cols-2 gap-2 [&>*]:text-left"
-              onClick={copyValue}
-            >
-              <button>{fullColor}</button>
-              <button>{shortColor}</button>
-            </div>
-          )}
+          <div
+            className="ml-4 grid flex-1 grid-cols-2 gap-2 [&>*]:text-left"
+            onClick={copyValue}
+          >
+            <button>{currentGlobalColorString}</button>
+            <button>
+              {transformShortColor(currentGlobalColorString, type)}
+            </button>
+          </div>
         </div>
       </div>
-
-      <p className="h-4 text-sm text-muted-foreground">{errorMessage}</p>
     </div>
   )
+}
+
+const parseColor = (colorString: string, type: SupportedColorType) => {
+  // switch (type) {
+  //   case 'oklch': {
+  //     console.log(colorString, 'colorString')
+  //     return new ColorIo(colorString)
+  //   }
+  // }
+
+  return new ColorIo(colorString)
+}
+
+const colorToColorString = (color: ColorIo, type: SupportedColorType) => {
+  switch (type) {
+    case 'lch': {
+      return `lch(${color.lch.map((val) => val.toFixed(2)).join(', ')})`
+    }
+    case 'hsl':
+      return Color(color.toString()).hsl().string()
+    case 'hex':
+      return Color(color.toString()).hex()
+    case 'oklch': {
+      return `oklch(${color.oklch.map((val) => val.toFixed(2)).join(', ')})`
+    }
+    case 'rgb': {
+      return color.toString()
+    }
+  }
+}
+
+const transformShortColor = (colorString: string, type: SupportedColorType) => {
+  const fn = {
+    hex: (val: string) => (val[0] === '#' ? val.slice(1) : val),
+    rgb: (val: string) => val.replace('rgb(', '').replace(')', ''),
+    hsl: (val: string) => val.replace('hsl(', '').replace(')', ''),
+    oklch: (val: string) => val.replace('oklch(', '').replace(')', ''),
+  }
+
+  return (fn as any)[type as any]?.(colorString) || colorString
 }
